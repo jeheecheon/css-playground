051c481262aa50d6c8a544723279cf6dba96cc09
jeheecheon <jeheecheon@gmail.com>
2025-03-05 21:19:23 +0900

    chore: install storybook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
 import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   { ignores: ["dist"] },
