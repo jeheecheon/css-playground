91357cb3a6be85558cced38949a5000266cd94c5
jeheecheon <jeheecheon@gmail.com>
2025-03-06 01:27:35 +0900

    chore: use lambda for FC

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import App from "@/App.tsx";
+import App from "@/App";
 import "@/globals.css";
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 );
